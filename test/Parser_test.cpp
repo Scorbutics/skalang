485773180682b6dbbd975fc5181cd041ac240d86
Scorbutics <brice.bulgarelli@gmail.com>
2018-08-11 16:00:51 +0200

    Finalisation TU sur Parser

--- test/Parser_test.cpp
+++ test/Parser_test.cpp
@@ -13,5 +13,36 @@ TEST_CASE("test") {
 	auto p = ska::Parser { keywords, reader };
 	auto ast = p.parse();
 	
-	std::cout << ast.first->asString() << std::endl;
+	auto& tree = *ast.first;
+	
+	CHECK(tree.size() == 4);
+	const auto& declaration = tree[0];
+	CHECK(declaration.op == ska::Operator::VARIABLE_DECLARATION);
+	CHECK(declaration.size() == 2);
+	CHECK(declaration[0].token == ska::Token { "i", ska::TokenType::IDENTIFIER });
+	CHECK(declaration[1].token == ska::Token { "0", ska::TokenType::DIGIT });
+
+	const auto& checkStatement = tree[1];
+	CHECK(checkStatement.op == ska::Operator::BINARY);
+	CHECK(checkStatement.size() == 2);
+	CHECK(checkStatement[0].token == ska::Token { "i", ska::TokenType::IDENTIFIER });
+	CHECK(checkStatement.token == ska::Token { "<", ska::TokenType::SYMBOL });
+	CHECK(checkStatement[1].token == ska::Token { "5", ska::TokenType::DIGIT });
+
+	const auto& incrementStatement = tree[2];
+	CHECK(incrementStatement.op == ska::Operator::UNARY);
+	CHECK(incrementStatement.size() == 1);
+	CHECK(incrementStatement[0].token == ska::Token { "i", ska::TokenType::IDENTIFIER });
+	
+	const auto& loopStatementBody = tree[3];
+	CHECK(loopStatementBody.op == ska::Operator::BLOCK);
+	CHECK(loopStatementBody.size() == 1);
+	CHECK(loopStatementBody[0].op == ska::Operator::FUNCTION_CALL);
+	const auto& functionCallStatement = loopStatementBody[0];
+
+	CHECK(functionCallStatement.token == ska::Token { "lol", ska::TokenType::IDENTIFIER });
+	CHECK(functionCallStatement.size() == 2);
+	CHECK(functionCallStatement[0].token == ska::Token { "\"mdr\"", ska::TokenType::STRING });
+	CHECK(functionCallStatement[1].token == ska::Token { "12", ska::TokenType::DIGIT });
+	
 }
