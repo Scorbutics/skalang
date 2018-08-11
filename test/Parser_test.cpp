b0205ec50f21fe49da6ddabdb5403816e0227cbd
Scorbutics <brice.bulgarelli@gmail.com>
2018-08-11 16:37:05 +0200

    TU sur Parser : debut des expressions

--- test/Parser_test.cpp
+++ test/Parser_test.cpp
@@ -37,12 +37,52 @@ TEST_CASE("test") {
 	const auto& loopStatementBody = tree[3];
 	CHECK(loopStatementBody.op == ska::Operator::BLOCK);
 	CHECK(loopStatementBody.size() == 1);
-	CHECK(loopStatementBody[0].op == ska::Operator::FUNCTION_CALL);
 	const auto& functionCallStatement = loopStatementBody[0];
 
+	CHECK(functionCallStatement.op == ska::Operator::FUNCTION_CALL);
 	CHECK(functionCallStatement.token == ska::Token { "lol", ska::TokenType::IDENTIFIER });
 	CHECK(functionCallStatement.size() == 2);
 	CHECK(functionCallStatement[0].token == ska::Token { "\"mdr\"", ska::TokenType::STRING });
 	CHECK(functionCallStatement[1].token == ska::Token { "12", ska::TokenType::DIGIT });
+}
+
+std::unique_ptr<ska::ASTNode> ASTFromInput(const std::string& input, const ska::ReservedKeywordsPool& keywords) {
+	auto t = ska::Tokenizer {keywords, input};
+	auto tokens = t.tokenize();
+	auto reader = ska::TokenReader { tokens };
+	auto p = ska::Parser { keywords, reader };
+	return std::move(p.parse().first);
+}
+
+TEST_CASE("Expression and priorities") {
+	const auto keywords = ska::ReservedKeywordsPool {};
+	SUBCASE("Simple mul") {
+		auto astPtr = ASTFromInput("5 * 2;", keywords);
+		auto& ast = *astPtr;
+		CHECK(ast.op == ska::Operator::BINARY);
+		CHECK(ast.size() == 2);
+		CHECK(ast.token == ska::Token { "*", ska::TokenType::SYMBOL });
+		CHECK(ast[0].token == ska::Token { "5", ska::TokenType::DIGIT });
+		CHECK(ast[1].token == ska::Token { "2", ska::TokenType::DIGIT });
+	}
+
+	SUBCASE("Syntax error : not an expression") {
+		auto toCheck = true;
+		try {
+			ASTFromInput("5 * 2", keywords);
+		} catch (std::exception& e) {
+			toCheck = false;
+		}
+		CHECK(!toCheck);
+	}
 	
+	SUBCASE("Simple div") {
+		auto astPtr = ASTFromInput("5 / 2;", keywords);
+		auto& ast = *astPtr;
+		CHECK(ast.op == ska::Operator::BINARY);
+		CHECK(ast.size() == 2);
+		CHECK(ast.token == ska::Token { "/", ska::TokenType::SYMBOL });
+		CHECK(ast[0].token == ska::Token { "5", ska::TokenType::DIGIT });
+		CHECK(ast[1].token == ska::Token { "2", ska::TokenType::DIGIT });
+	}
 }
