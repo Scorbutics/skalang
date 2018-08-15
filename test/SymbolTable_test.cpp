abb1297a575f813ba4ec83a1861cf0207b7ad8f6
Scorbutics <brice.bulgarelli@gmail.com>
2018-08-15 11:42:01 +0200

    Suite TUs SymbolTable

--- test/SymbolTable_test.cpp
+++ test/SymbolTable_test.cpp
@@ -19,13 +19,22 @@ std::unique_ptr<ska::ASTNode> ASTFromInput(const std::string& input, SymbolTable
 
 TEST_CASE("test") {
 	SymbolTablePtr table_test;
-	auto astPtr = ASTFromInput("var i = 0; { var toto = 2; var i = 9; }", table_test);
+	auto astPtr = ASTFromInput("var i = 0; var titi = \"llllll\"; { var toto = 2; var i = 9; }", table_test);
 	auto& table = *table_test;
 	
 	CHECK(table.nested().size() == 1);
 	auto nestedI = (*table.nested()[0])["i"];
 	auto i = table["i"];
+	auto nestedToto = (*table.nested()[0])["toto"];
+	auto toto = table["toto"];
+	auto titi = table["titi"];
+	auto nestedTiti = (*table.nested()[0])["titi"];
+
 	CHECK(i != nullptr);
 	CHECK(nestedI  != nullptr);
 	CHECK(i != nestedI);
+	CHECK(toto == nullptr);
+	CHECK(nestedToto != nullptr);
+	CHECK(nestedTiti != nullptr);
+	CHECK(nestedTiti == titi);
 }
