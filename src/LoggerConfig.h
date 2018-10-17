92611e92792182fe3c0a3152fc2a6e3ae4ec53de
ebbu6050 <brice.bulgarelli-ext@pole-emploi.fr>
2018-10-17 12:07:48 +0200

    MAJ build scripts et compilation

--- src/LoggerConfig.h
+++ src/LoggerConfig.h
@@ -1,4 +1,5 @@
 #pragma once
 #include <Logging/Logger.h>
+#include <Logging/ColorStream.h>
 
-#define SLOG(logger, level, currentClass) SKA_LOGC(logger, level, currentClass) << SKA_CURRENT_FUNCTION << "(" << __FILE__ << " " << __LINE__ << ") "
+#define SLOG(logger, level, currentClass) SKA_LOGC(logger, level, currentClass) << SKA_CURRENT_FUNCTION << ska::EnumColorStream::BROWN << " (" << __FILE__ << " " << __LINE__ << ") " << ska::EnumColorStream::WHITE
--- src/TypeBuilder.cpp
+++ src/TypeBuilder.cpp
@@ -20,36 +20,35 @@ namespace ska {
 
 std::ofstream TypeBuilderLogFileOutput {"TypeBuilderLogError.txt" };
 
-ska::Logger<ska::LogLevel::Warn, ska::LogLevel::Warn, ska::LogAsync> TypeBuilderLogger {std::cout, [](const ska::LogEntry& entry) {
-    return entry.getLogLevel() == ska::LogLevel::Warn;
-}};
+ska::Logger<ska::LogLevel::Info, ska::LogLevel::Error, ska::LogAsync> TypeBuilderLoggerFile {TypeBuilderLogFileOutput};
+ska::Logger<ska::LogLevel::Error, ska::LogLevel::Error, ska::LogSync> TypeBuilderLoggerConsole {std::cout};
 
 ska::TypeBuilder::TypeBuilder(Parser& parser, const SymbolTable& symbolTable) : 
     m_symbols(symbolTable),
     SubObserver<ExpressionTokenEvent>(std::bind(&TypeBuilder::matchExpression, this, std::placeholders::_1), parser) {
 	static auto done = false;
 	if (!done) {
-		TypeBuilderLogger.addOutputTarget(TypeBuilderLogFileOutput, [](const ska::LogEntry& entry) {
+		/*TypeBuilderLogger.addOutputTarget(TypeBuilderLogFileOutput, [](const ska::LogEntry& entry) {
 			return entry.getLogLevel() == ska::LogLevel::Error;
 		});
 		TypeBuilderLogger.setPattern(LogLevel::Error, "%10c[%h:%m:%s:%T]%12c[Error] %8c(%i) %14c%C %15c%v");
-		TypeBuilderLogger.setPattern(LogLevel::Warn, "%10c[%h:%m:%s:%T]%12c[Warn] %8c(%i) %14c%C %15c%v");
+		TypeBuilderLogger.setPattern(LogLevel::Warn, "%10c[%h:%m:%s:%T]%12c[Warn] %8c(%i) %14c%C %15c%v");*/
 		done = true;
 	}
 }
 
 bool ska::TypeBuilder::matchVariable(VarTokenEvent& event) const {
-	SLOG(TypeBuilderLogger, Info, ska::TypeBuilder) << "Building type for variable " << event.node.asString();
+	SLOG(TypeBuilderLogger, LogLevel::Info, ska::TypeBuilder) << "Building type for variable " << event.node.asString();
     TypeBuilderDispatchCalculation(m_symbols, event.node[0]);
-	SLOG(TypeBuilderLogger, Debug, ska::TypeBuilder) << "Type built " << event.node[0].type.value().asString();
+	SLOG(TypeBuilderLogger, LogLevel::Debug, ska::TypeBuilder) << "Type built " << event.node[0].type.value().asString();
 
     return true;
 }
 
 bool ska::TypeBuilder::matchExpression(ExpressionTokenEvent& event) const {
-	SLOG(TypeBuilderLogger, Warn, ska::TypeBuilder) << "Building type for expression " << event.node.asString();
+	SLOG(TypeBuilderLogger, LogLevel::Warn, ska::TypeBuilder) << "Building type for expression " << event.node.asString();
 	TypeBuilderDispatchCalculation(m_symbols, event.node);
-	SLOG(TypeBuilderLogger, Error, ska::TypeBuilder) << "Type built " << event.node.type.value().asString();
+	SLOG(TypeBuilderLogger, LogLevel::Error, ska::TypeBuilder) << "Type built " << event.node.type.value().asString();
     return true;
 }
 
@@ -58,9 +57,9 @@ bool ska::TypeBuilder::matchFunction(FunctionTokenEvent& event) const {
         default: break;
         
         case FunctionTokenEventType::DECLARATION_PARAMETERS: {			
-			SLOG(TypeBuilderLogger, Info, ska::TypeBuilder) << "Building type for parameter declaration " << event.node.asString();
+			SLOG(TypeBuilderLogger, LogLevel::Info, ska::TypeBuilder) << "Building type for parameter declaration " << event.node.asString();
             TypeBuilderDispatchCalculation(m_symbols, event.node);
-			SLOG(TypeBuilderLogger, Info, ska::TypeBuilder) << "Type built " << event.node.type.value().asString();
+			SLOG(TypeBuilderLogger, LogLevel::Info, ska::TypeBuilder) << "Type built " << event.node.type.value().asString();
         } break;
 
     }
