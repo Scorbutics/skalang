67d189574794411f3b9d5ac27630d60573c8c5f3
ebbu6050 <brice.bulgarelli-ext@pole-emploi.fr>
2018-10-17 18:07:57 +0200

    Avancee sur logging complexe (multiple loggers)

--- src/LoggerConfig.h
+++ src/LoggerConfig.h
@@ -1,5 +1,22 @@
 #pragma once
 #include <Logging/Logger.h>
-#include <Logging/ColorStream.h>
 
-#define SLOG(logger, level, currentClass) SKA_LOGC(logger, level, currentClass) << SKA_CURRENT_FUNCTION << ska::EnumColorStream::BROWN << " (" << __FILE__ << " " << __LINE__ << ") " << ska::EnumColorStream::WHITE
+namespace detail {
+	
+	template <class ... Logger>
+	class LogCapturer {
+	public:	
+		template <ska::LogLevel level, class CurrentClass>
+		auto log(const char* functionName, const char* filename, const char* line) {
+			
+		}
+		
+	private:
+		template <ska::LogLevel level, class CurrentClass>
+		auto logForLogger(auto& logger, const char* functionName, 
+	
+		std::tuple<Logger...> m_loggers;
+	};
+}
+
+#define SLOG(level, currentClass) detail::LogCapturer::log<level, currentClass>(SKA_CURRENT_FUNCTION, __FILE__ ,__LINE__)
