3895a847ff4967324adecaf606e18dbe6200f162
ebbu6050 <brice.bulgarelli-ext@pole-emploi.fr>
2018-10-26 17:38:11 +0200

    Upgrade logs

--- src/LoggerConfig.cpp
+++ src/LoggerConfig.cpp
@@ -11,10 +11,10 @@ detail::SkaLangLogger detail::BuildLogger(const char * filename) {
 	logger.get<0>().addOutputTarget(TypeBuilderLogFileOutput);
 	logger.get<1>().addOutputTarget(std::cout);
  
-	logger.get<1>().setPattern(ska::LogLevel::Debug, "%10c[%h:%m:%s:%T]%9c[Debug]%14c(%10f l.%3l) %15c%v");
-	logger.get<1>().setPattern(ska::LogLevel::Info, "%10c[%h:%m:%s:%T]%10c[Info ]%14c(%10f l.%3l) %15c%v");
-	logger.get<1>().setPattern(ska::LogLevel::Warn, "%10c[%h:%m:%s:%T]%11c[Warn ]%14c(%10f l.%3l) %15c%v");
-	logger.get<1>().setPattern(ska::LogLevel::Error, "%10c[%h:%m:%s:%T]%12c[Error]%14c(%10f l.%3l) %15c%v");
+	/*logger.get<1>().setPattern(ska::LogLevel::Debug, "%10c[%h:%m:%s:%T]%9c[Debug]%14c(%10F l.%3l) %15c%v");
+	logger.get<1>().setPattern(ska::LogLevel::Info, "%10c[%h:%m:%s:%T]%10c[Info ]%14c(%10F l.%3l) %15c%v");
+	logger.get<1>().setPattern(ska::LogLevel::Warn, "%10c[%h:%m:%s:%T]%11c[Warn ]%14c(%10F l.%3l) %15c%v");
+	logger.get<1>().setPattern(ska::LogLevel::Error, "%10c[%h:%m:%s:%T]%12c[Error]%14c(%10F l.%3l) %15c%v");*/
 
     ska::process::SignalHandlerAddAction([](int signalCode) {
         detail::Logger.terminate();
