930d8aaebf0cf593aa1b8f1d7cfcc9f958fe5770
Scorbutics <brice.bulgarelli@outlook.com>
2019-04-28 23:50:16 +0200

    test: adding some scripts & bindings in prevision of using ECS

--- runner/src/main.cpp
+++ runner/src/main.cpp
@@ -38,15 +38,44 @@ int main(int argc, char* argv[]) {
 	}
 	
 	auto scriptEmBinding = ska::ScriptBridge{ scriptCache, "em_lib", typeBuilder, symbolsTypeUpdater, reservedKeywords };
-	
-	auto memInputComponent = scriptEmBinding.createMemory();
 	scriptEmBinding.bindFunction("setInputMovePower", std::function<void(int, int)>([](int characterId, int value) {
 		std::cout << "move power for " << characterId << " is now " << value << std::endl;
 	}));
 	scriptEmBinding.bindFunction("setInputJumpPower", std::function<void(int, int)>([](int characterId, int value) {
 		std::cout << "jump power for " << characterId << " is now " << value << std::endl;
 	}));
+	scriptEmBinding.bindFunction("removeInputComponent", std::function<void(int)>([](int characterId) { }));
+	scriptEmBinding.bindFunction("restoreInputComponent", std::function<void(int, int, int)>([](int characterId, int movePower, int jumpPower) {
+		std::cout << "restoring input with move " << movePower << " and jump " << jumpPower << std::endl;
+	}));
 	scriptEmBinding.buildFunctions();
+
+	auto scriptParametersBinding = ska::ScriptBridge{ scriptCache, "parameters_gen_lib", typeBuilder, symbolsTypeUpdater, reservedKeywords };
+	scriptParametersBinding.import(parser, interpreter, { {"Parameters", "parameters"} });
+	scriptParametersBinding.bindGenericFunction("Gen", { "Parameters::Fcty" }, std::function<ska::NodeValue(std::vector<ska::NodeValue>)>([&](std::vector<ska::NodeValue> params) -> ska::NodeValue {
+		auto result = scriptParametersBinding.createMemory();
+		result->emplace("asInt", std::make_unique<ska::BridgeFunction>(std::function<ska::NodeValue(std::vector<ska::NodeValue>)>([&](std::vector<ska::NodeValue> params) {
+			auto index = params[0].nodeval<int>();
+			return ska::NodeValue{ index };
+		})));
+		result->emplace("asString", std::make_unique<ska::BridgeFunction>(std::function<ska::NodeValue(std::vector<ska::NodeValue>)>([&](std::vector<ska::NodeValue> params) {
+			return ska::NodeValue{ "toto" };
+		})));
+		return result;
+	}));
+	scriptParametersBinding.bindFunction("asString", std::function<std::string(int)>([](int index) {
+		return "";
+	}));
+	scriptParametersBinding.buildFunctions();
+
+	auto scriptCharacterCommands = ska::ScriptBridge{ scriptCache, "character_commands_lib", typeBuilder, symbolsTypeUpdater, reservedKeywords };
+	scriptCharacterCommands.bindFunction("jump", std::function<void(int)>([](int index) {
+		std::cout << "jump ! " << index << std::endl;
+	}));
+	scriptCharacterCommands.bindFunction("move", std::function<void(int, int, int)>([](int direction, int power, int t) {
+		std::cout << "move ! direction " << direction << " power " << power << " ??? " << t << std::endl;
+	}));
+	scriptCharacterCommands.buildFunctions();
 
 	auto scriptCharacterBinding = ska::ScriptBridge{ scriptCache, "character_generator", typeBuilder, symbolsTypeUpdater, reservedKeywords };
 	scriptCharacterBinding.import(parser, interpreter, { {"Character", "character"} });
@@ -61,14 +90,16 @@ int main(int argc, char* argv[]) {
 	scriptBinding.bindFunction("printInt", std::function<void(int)>([](int value) {
 		std::cout << value << std::endl;
 	}));
-	
 	scriptBinding.bindFunction("printString", std::function<void(std::string)>([](std::string value) {
 		std::cout << value << std::endl;
+	}));
+	scriptBinding.bindFunction("wait", std::function<void(int)>([](int value) {
+		std::cout << "wait " << value << std::endl;
 	}));
 	scriptBinding.buildFunctions();
 
 	auto executor = ska::Script{ scriptCache, "main", ska::Tokenizer{ reservedKeywords, 
-	"var Player = import \"" + std::string{argv[1]} + "\"; var CharacterGenerator = import \"character_generator\"; Player.run(CharacterGenerator.Gen());"
+	"var Player = import \"" + std::string{argv[1]} + "\"; var CharacterGenerator = import \"character_generator\"; var ParametersGenerator = import \"parameters_gen_lib\"; Player.run(CharacterGenerator.Gen(), ParametersGenerator.Gen());"
 	}.tokenize() };
 
 	try {
