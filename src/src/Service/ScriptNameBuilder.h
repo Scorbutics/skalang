e0a534eec099130c6877ff3f09cdc04878765e25
Brice BULGARELLI <brice.bulgarelli.prestataire@sfr.com>
2019-06-17 17:43:46 +0200

    refactor : clean-up & fixes ScriptNameBuilder

--- src/src/Service/ScriptNameBuilder.h
+++ src/src/Service/ScriptNameBuilder.h
@@ -9,7 +9,8 @@ namespace ska {
     enum class ScriptNameStrategy {
         WORKING_DIRECTORY,
         RELATIVE_TO_PARENT,
-        RELATIVE_TO_RUNNER
+        RELATIVE_TO_RUNNER,
+        STD_DIRECTORY
     };
 
     std::string& ScriptNameStandardLibraryPrefix();
@@ -20,9 +21,9 @@ namespace ska {
     }
 
     //Executable relative path
-    inline std::string ScriptNameRelativeToRunnerDirectory(const std::string& scriptCalled) {
+    inline std::string ScriptNameRelativeToRunnerDirectory(const std::string& scriptCalled, const std::string& stdPrefixPath = ScriptNameStandardLibraryPrefix()) {
         const auto fileNameData = FileNameData {FileUtils::getExecutablePath()};
-        return FileUtils::getCanonicalPath(fileNameData.path + "/" + ScriptNameStandardLibraryPrefix() + "/" + scriptCalled + "." SKALANG_SCRIPT_EXT);
+        return FileUtils::getCanonicalPath(fileNameData.path + "/" + (stdPrefixPath.empty() ? "" : (stdPrefixPath + "/")) + scriptCalled + "." SKALANG_SCRIPT_EXT);
     }
 
     //File relative path
@@ -35,44 +36,5 @@ namespace ska {
         return FileUtils::getCanonicalPath(fileNameData.path + "/" + scriptCalled + "." SKALANG_SCRIPT_EXT);
     }
 
-
-
-    inline std::string ScriptNameDeduce(const std::string& scriptCaller, const std::string& scriptCalled) {
-        if(FileUtils::isAbsolutePath(scriptCalled)) {
-            const auto lastDotPos = scriptCalled.find_last_of('.');
-            const auto ext = (lastDotPos == std::string::npos || scriptCalled.substr(lastDotPos) != "." SKALANG_SCRIPT_EXT) ? "." SKALANG_SCRIPT_EXT : "";
-            const auto fileScriptCalled = scriptCalled + ext;
-            return FileUtils::getCanonicalPath(fileScriptCalled);
-        }
-
-        auto strategy = ScriptNameStrategy::RELATIVE_TO_PARENT;
-        auto scriptCalledName = std::string{};
-        const auto namespaceSeparatorPosition = scriptCalled.find_first_of(':');
-        if(namespaceSeparatorPosition != std::string::npos) {
-            auto namespace_ = scriptCalled.substr(0, namespaceSeparatorPosition);
-            if(namespace_ == "std") {
-                strategy = ScriptNameStrategy::RELATIVE_TO_RUNNER;
-            } else if(namespace_ == "wd") {
-                strategy = ScriptNameStrategy::WORKING_DIRECTORY;
-			} else if (namespace_ == "bind") {
-				strategy = ScriptNameStrategy::RELATIVE_TO_RUNNER;
-			} else {
-				throw std::runtime_error("undefined namespace \"" + namespace_ + "\"used in script declaration \"" + scriptCalled + "\"");
-			}
-            scriptCalledName = scriptCalled.substr(namespaceSeparatorPosition + 1);
-        } else {
-            scriptCalledName = scriptCalled;
-        }
-
-        switch (strategy) {
-        case ScriptNameStrategy::WORKING_DIRECTORY:
-            return ScriptNameCurrentWorkingDirectory(scriptCalledName);
-        
-        case ScriptNameStrategy::RELATIVE_TO_RUNNER:
-            return ScriptNameRelativeToRunnerDirectory(scriptCalledName);
-
-        default:
-            return ScriptNameRelativeToParentDirectory(scriptCaller, scriptCalledName);
-        }
-    }
+    std::string ScriptNameDeduce(const std::string& scriptCaller, const std::string& scriptCalled, const std::string& stdPrefixPath = ScriptNameStandardLibraryPrefix());
 }
