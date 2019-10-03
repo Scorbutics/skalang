e13e7954a14a9feff8de30154d65099967292d78
Brice BULGARELLI <brice.bulgarelli.prestataire@sfr.com>
2019-10-03 15:53:33 +0200

    fix : push front array

--- bytecode/interpreter/src/BytecodeInterpreter/Units/InterpreterCommandPushFArr.cpp
+++ bytecode/interpreter/src/BytecodeInterpreter/Units/InterpreterCommandPushFArr.cpp
@@ -3,6 +3,6 @@
 SKALANG_BYTECODE_INTERPRETER_COMMAND_DECLARE(PUSH_F_ARR)(ExecutionContext& context, const Value& left, const Value& right) {
   auto toPush = context.getCell(left);
 	auto container = context.get<NodeValueArray>(right);
-  container->push_back(toPush);
+  container->push_front(toPush);
 	return container;
 }
