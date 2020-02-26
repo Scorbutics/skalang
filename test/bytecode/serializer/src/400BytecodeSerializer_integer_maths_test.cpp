78d016ca94e020dcb058c8190543046b5a94a6e4
Brice BULGARELLI <brice.bulgarelli.prestataire@sfr.com>
2020-02-26 15:12:41 +0100

    fix : unvoluntary committed piece of code

--- test/bytecode/serializer/src/400BytecodeSerializer_integer_maths_test.cpp
+++ test/bytecode/serializer/src/400BytecodeSerializer_integer_maths_test.cpp
@@ -5,26 +5,6 @@
 #include "Config/LoggerConfigLang.h"
 #include "BytecodeSerializerTest.h"
 
-template <class T>
-T& build(const std::string& scriptName) {
-	const auto foundExtPos = scriptName.rfind("." SKALANG_SCRIPT_EXT);
-	auto ext = std::string{ "b" };
-	if (foundExtPos == std::string::npos) {
-		ext = "." SKALANG_SCRIPT_EXT + ext;
-	}
-	auto bytecodeScriptName = scriptName + ext;
-	static auto tabs = std::unordered_map<std::string, T>{};
-	auto tabFound = tabs.find(bytecodeScriptName);
-	if (tabFound == tabs.end()) {
-		tabFound = tabs.emplace(bytecodeScriptName, T{}).first;
-		tabFound->second.open(bytecodeScriptName, std::iostream::binary);
-		if (tabFound->second.fail()) {
-			throw std::runtime_error("opening file " + bytecodeScriptName + " failed");
-		}
-	}
-	return tabFound->second;
-}
-
 static std::stringstream SerializingStream = std::stringstream{};
 
 ska::bytecode::SerializationStrategy SerializeInStream() {
@@ -66,8 +46,6 @@ TEST_CASE("[BytecodeSerializer] literal alone") {
 	// A way to test instructions emptyness
 	CHECK(destinationCache.at(0).begin() == destinationCache.at(0).end());
 }
-
-
 
 TEST_CASE("[BytecodeSerializer] var declaration") {
 	auto [script, data] = Serialize("var toto = 4;");
