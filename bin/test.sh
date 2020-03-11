c0aca5e9fbbbf9c57a90c1277c70a0c5bc779331
Brice BULGARELLI <brice.bulgarelli.prestataire@sfr.com>
2020-03-11 15:13:08 +0100

    chore : improve test.sh

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 BASEDIR=$(dirname "$0")
-echo "$BASEDIR"
+echo "In '$BASEDIR'"
 
-$BASEDIR/skalang_test
-$BASEDIR/skalang_bytecode_test
+$BASEDIR/skalang_test 1> /dev/null && \
+$BASEDIR/skalang_bytecode_generator_test 1> /dev/null && \
+$BASEDIR/skalang_bytecode_interpreter_test 1> /dev/null && \
+echo "SUCCESS" || echo "FAILURE"
