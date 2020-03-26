abef86364dc997698163a313eda67b28992123ef
Scorbutics <brice.bulgarelli@gmail.com>
2020-03-26 01:05:52 +0100

    chore : toString representation changes

--- test/bytecode/generator/src/BytecodeCompare.cpp
+++ test/bytecode/generator/src/BytecodeCompare.cpp
@@ -2,16 +2,27 @@
 #include "BytecodeCompare.h"
 #include "Generator/Value/BytecodeScriptGeneration.h"
 
+static std::string HandleScriptSuffix(std::size_t scriptId, std::string str) {
+    if (str.empty() || str[0] != 'V' && str[0] != 'R') {
+        return str;
+    }
+    const auto dotPos = str.find_last_of(':');
+    if (dotPos == std::string::npos) {
+        return str + ":" + std::to_string(scriptId);
+    }
+    return str;
+}
+
 void BytecodeCompare(const ska::bytecode::ScriptGeneration& scriptGeneration, std::vector<ska::bytecode::BytecodePart> expected) {
-  auto index = std::size_t {scriptGeneration.id()};
+  auto index = std::size_t {0};
   CHECK(scriptGeneration.size() == expected.size());
   for(const auto& r : scriptGeneration) {
 	const auto equality =
   	index < expected.size() &&
   	r.command() == expected[index].command &&
-  	r.dest().toString() == expected[index].dest &&
-  	r.left().toString() == expected[index].left &&
-  	r.right().toString() == expected[index].right;
+  	r.dest().toString() == HandleScriptSuffix(scriptGeneration.id(), expected[index].dest) &&
+  	r.left().toString() == HandleScriptSuffix(scriptGeneration.id(), expected[index].left) &&
+  	r.right().toString() == HandleScriptSuffix(scriptGeneration.id(), expected[index].right);
 	CHECK(equality);
 	index++;
   }
