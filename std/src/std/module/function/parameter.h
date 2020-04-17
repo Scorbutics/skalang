46b4442b844fdbfe55984f6e64b9ed3e1ee0702f
Scorbutics <brice.bulgarelli@gmail.com>
2020-04-17 19:59:38 +0200

    feature : update the runner script and parameter module

--- std/src/std/module/function/parameter.h
+++ std/src/std/module/function/parameter.h
@@ -13,28 +13,20 @@ namespace ska {
         template <class Interpreter>
         class ParameterModule : public Module<Interpreter> {
         public:
-            ParameterModule(ModuleConfiguration<Interpreter>& config, const std::vector<NodeValue>& parameterValues) :
+            ParameterModule(ModuleConfiguration<Interpreter>& config, const NodeValueArray& parameterValues) :
                 Module<Interpreter> { config, "std.native.function.parameter", "std:std.function.parameter" },
-                m_constructor(BridgeBuilder<Interpreter> { Module<Interpreter>::m_bridge, "Fcty" }),
+                m_constructor(BridgeBuilder<Interpreter> { Module<Interpreter>::m_bridge }),
 				m_parameters(parameterValues) {
 
-                m_constructor.bindField("asInt", [&](std::vector<ska::NodeValue> params) -> ska::NodeValue {
-                    const std::size_t index = params[0].nodeval<long>();
-                    return m_parameters.size() > index ? static_cast<long>(m_parameters[index].convertNumeric()) : -1;
-                });
-                m_constructor.bindField("asString", [&](std::vector<ska::NodeValue> params) -> ska::NodeValue {
-                    const std::size_t index = params[0].nodeval<long>();
-                    return std::make_shared<std::string>(m_parameters.size() > index ? m_parameters[index].convertString() : "");
-                });
-                m_constructor.bindField("size", [&](std::vector<ska::NodeValue> buildParams) -> ska::NodeValue {
-                    return static_cast<long>(m_parameters.size());
+                m_constructor.bindField("Get", [&](std::vector<ska::NodeValue>) -> ska::NodeValue {
+                    return m_parameters;
                 });
                 m_constructor.generate();
             }
 
             ~ParameterModule() override = default;
         private:
-            const std::vector<NodeValue>& m_parameters;
+            const NodeValueArray& m_parameters;
             BridgeBuilder<Interpreter> m_constructor;
         };
     }
